f27cad6d181e1292b0170e332bd187d0950d1cd8
rprem <anandalok09@gmail.com>
2025-04-13 22:36:12 +0530

    created queseservice

---
+++ queseservice/src/main/java/com/quese/queseservice/QueseserviceApplication.java
@@ -0,0 +1,13 @@
+package com.quese.queseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueseserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueseserviceApplication.class, args);
+	}
+
+}
