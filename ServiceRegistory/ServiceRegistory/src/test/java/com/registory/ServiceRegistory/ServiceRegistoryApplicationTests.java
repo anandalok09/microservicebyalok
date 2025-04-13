b6f33338f1924f1bd6a3a4397635238f7011c05d
rprem <anandalok09@gmail.com>
2025-04-13 22:43:23 +0530

    ServiceRegistory

---
+++ ServiceRegistory/ServiceRegistory/src/test/java/com/registory/ServiceRegistory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registory.ServiceRegistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
